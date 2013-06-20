9c1764f23382d0a3ea91f50b4326a4ebcafcf24d
Necromos <masternecromos@gmail.com>
2013-06-20 22:53:55 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gw2pc::Application.config.secret_key_base = '70e3e08a54760f65605a9f924cacb6e6d86732d5edf2504ac08f329343ab85c4c1b2a3ae855c03c489624ee4d76eebe83f65de600558378495267fdf6a65f79c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gw2pc::Application.config.session_store :cookie_store, key: '_gw2pc_session'
